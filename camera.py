8acbe7b91c8340ee729b0d2d2be72b4eff5fea23
Broderick Cotter <79217096+M3kko@users.noreply.github.com>
2024-02-11 22:29:31 -0600

    Update camera.py
    optimized code

--- camera.py
+++ camera.py
@@ -3,7 +3,7 @@ import cv2 as cv
 class Camera:
 
     def __init__(self):
-        self.camera = cv.VideoCapture(0)
+        self.camera = cv.VideoCapture(1)
         if not self.camera.isOpened():
             raise ValueError("Unable to open camera!")
 

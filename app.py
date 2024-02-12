ff1ae5e266ace082c45e77df4d046dbc693049f2
Broderick Cotter <79217096+M3kko@users.noreply.github.com>
2024-02-11 22:29:54 -0600

    Update app.py
    REALLY optimized the code

--- app.py
+++ app.py
@@ -1,110 +1,107 @@
 import tkinter as tk
 from tkinter import simpledialog
-import cv2 as cv
+import cv2 
 import os
 import PIL.Image, PIL.ImageTk
 import model
 import camera
 
 class App:
-    def __init__(self, window=None, window_title="Camera Classifier"):
-        if window is None:
-            self.window = tk.Tk()
-        else:
-            self.window = window
-        self.window.title(window_title)
+
+    def __init__(self):
+
+        self.window = tk.Tk()
+        self.window_title = "Rep Counter"
 
         self.counters = [1, 1]
+        self.rep_counter = 0
+
+        self.extended = False
+        self.contracted = False
+        self.last_prediction = 0
+
         self.model = model.Model()
-        self.auto_predict = False
+
+        self.counting_enabled = False
+
         self.camera = camera.Camera()
-        
+
         self.init_gui()
-        
+
         self.delay = 15
         self.update()
-        
+
         self.window.attributes("-topmost", True)
         self.window.mainloop()
-
+        
     def init_gui(self):
         self.canvas = tk.Canvas(self.window, width=self.camera.width, height=self.camera.height)
         self.canvas.pack()
 
-        self.btn_toggleauto = tk.Button(self.window, text="Auto Prediction", width=50, command=self.auto_predict_toggle)
+        self.btn_toggleauto = tk.Button(self.window, text="Toggle Counting", width=50, command=self.counting_toggle)
         self.btn_toggleauto.pack(anchor=tk.CENTER, expand=True)
 
-        # Prompting for class names at the start might not be the best UX in a GUI application
-        # Consider setting default names or creating a settings dialog to handle this
-        self.classname_one = "Class 1"
-        self.classname_two = "Class 2"
-
-        self.btn_class_one = tk.Button(self.window, text=self.classname_one, width=50, command=lambda: self.save_for_class(1))
+        self.btn_class_one = tk.Button(self.window, text="Extended", width=50, command=lambda: self.save_for_class(1))
         self.btn_class_one.pack(anchor=tk.CENTER, expand=True)
 
-        self.btn_class_two = tk.Button(self.window, text=self.classname_two, width=50, command=lambda: self.save_for_class(2))
+        self.btn_class_two = tk.Button(self.window, text="Contracted", width=50, command=lambda: self.save_for_class(2))
         self.btn_class_two.pack(anchor=tk.CENTER, expand=True)
-
-        self.btn_train = tk.Button(self.window, text="Train Model", width=50, command=self.train_model)
+        
+        self.btn_train = tk.Button(self.window, text="Train Model", width=50, command=lambda: self.model.train_model(self.counters))
         self.btn_train.pack(anchor=tk.CENTER, expand=True)
-
-        self.btn_predict = tk.Button(self.window, text="Predict", width=50, command=self.predict)
-        self.btn_predict.pack(anchor=tk.CENTER, expand=True)
 
         self.btn_reset = tk.Button(self.window, text="Reset", width=50, command=self.reset)
         self.btn_reset.pack(anchor=tk.CENTER, expand=True)
 
-        self.class_label = tk.Label(self.window, text="CLASS")
-        self.class_label.config(font=("Arial", 20))
-        self.class_label.pack(anchor=tk.CENTER, expand=True)
+        self.counter_label = tk.Label(self.window, text=f"{self.rep_counter}")
+        self.counter_label.config(font=("Arial", 24))
+        self.counter_label.pack(anchor=tk.CENTER, expand=True)
 
-    def auto_predict_toggle(self):
-        self.auto_predict = not self.auto_predict
+    def update(self):
+       if self.counting_enabled:
+           self.predict()
+           
+       if self.extended and self.contracted:
+           self.extended, self.contracted = False, False
+           self.rep_counter += 1
+
+       self.counter_label.config(text=f"{self.rep_counter}")
+
+       ret, frame = self.camera.get_frame()
+       if ret: 
+           self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+           self.canvas.create_image(0,0, image=self.photo, anchor=tk.NW)
+
+       self.window.after(self.delay, self.update)   
+
+    def predict(self):
+        frame = self.camera.get_frame()
+        prediction = self.model.predict(frame)
+
+        if prediction != self.last_prediction:
+            if prediction == 1:
+                self.extended = True
+                self.last_prediction = 1
+            if prediction == 2:
+                self.contracted = True
+                self.last_prediction = 2
+
+    def counting_toggle(self):
+       self.counting_enabled = not self.counting_enabled
 
     def save_for_class(self, class_num):
         ret, frame = self.camera.get_frame()
-        if ret:
-            directory = str(class_num)
-            if not os.path.exists(directory):
-                os.mkdir(directory)
-            filepath = f'{directory}/frame{self.counters[class_num-1]}.jpg'
-            cv.imwrite(filepath, cv.cvtColor(frame, cv.COLOR_RGB2GRAY))
-            img = PIL.Image.open(filepath)
-            img.thumbnail((150, 150), PIL.Image.ANTIALIAS)
-            img.save(filepath)
-            self.counters[class_num - 1] += 1
+        if not os.path.exists("1"):
+            os.mkdir("1")
+        if not os.path.exists("2"):
+            os.mkdir("2")
 
-    def train_model(self):
-        self.model.train_model(self.counters)
+        cv2.imwrite(f"{class_num}/frame{self.counters[class_num-1]}.jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY))
+        img = PIL.Image.open(f"{class_num}/frame{self.counters[class_num-1]}.jpg")
+        img.thumbnail((150,150), PIL.Image.LANCZOS)
+        img.save(f"{class_num}/frame{self.counters[class_num-1]}.jpg")
+
+        self.counters[class_num-1] += 1
 
     def reset(self):
-        for folder in ['1', '2']:
-            for file in os.listdir(folder):
-                file_path = os.path.join(folder, file)
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-        self.counters = [1, 1]
-        self.model = model.Model()
-        self.class_label.config(text="CLASS")
-
-    def update(self):
-        if self.auto_predict:
-            self.predict()
-
-        ret, frame = self.camera.get_frame()
-        if ret:
-            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-            self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-        self.window.after(self.delay, self.update)
-
-    def predict(self):
-        ret, frame = self.camera.get_frame()
-        if ret:
-            prediction = self.model.predict(frame)
-            if prediction == 1:
-                self.class_label.config(text=self.classname_one)
-            elif prediction == 2:
-                self.class_label.config(text=self.classname_two)
-
-if __name__ == "__main__":
-    App()
+        self.rep_counter = 0
